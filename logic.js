151ba0901d13024f22dc8207303605c1f0561cac
angie88 <angieyeh24@gmail.com>
2016-06-01 20:32:29 -0700

    passing values around from callback function to callback function

--- logic.js
+++ logic.js
@@ -1,13 +1,33 @@
 var fs = require('fs');
+var path = require('path');
 
 function getFileNames(directory){
   fs.readdir(directory, function(err, files) {
-    console.log(files)
-    var filtered = files.filter(function(elem) {
-      return elem.slice(-2) === 'js';
+    var jsFiles = files.filter(function(elem) {
+      return elem.slice(-2) === 'js' || elem.slice(-3) === '.jsx';
     });
-    console.log(filtered);
-  })
+    var allDirs = files.filter(function(dir) {
+      return fs.statSync(path.join(directory, dir)).isDirectory();
+    })
+    return ignoreDirectories(jsFiles, allDirs, filesToRead);
+  });
+}
+
+getFileNames(__dirname);
+
+function ignoreDirectories(jsFiles, allDirs, callback) {
+  fs.readFile(__dirname + '/.gitignore', 'utf8', function(err, data) {
+    if (err) throw err;
+    var ignoredDirs = data.split('\n')
+    ignoredDirs.pop()
+    ignoredDirs = ignoredDirs.concat(['.git']);
+    filesToRead(ignoredDirs)
+
+  });
+}
+
+function filesToRead(userFiles, userDirs) {
+
 }
 
 module.exports = {
