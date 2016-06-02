1b7a62fa867b68c1dd21f934d1509b92a2ba1833
angie88 <angieyeh24@gmail.com>
2016-06-01 23:54:48 -0700

    getting all the .js/.jsx files we want to parse

--- logic.js
+++ logic.js
@@ -1,34 +1,54 @@
 var fs = require('fs');
 var path = require('path');
+var parseFiles = require('./parseFiles')
+var filesArray = [];
 
-function getFileNames(directory){
-  fs.readdir(directory, function(err, files) {
+function getFileNames(appPath) {
+  fs.readdir(appPath, function(err, files) {
     var jsFiles = files.filter(function(elem) {
       return elem.slice(-2) === 'js' || elem.slice(-3) === '.jsx';
     });
+    filesArray = filesArray.concat(jsFiles.map(function(files) {
+      return `${appPath}/${files}`;
+    }));
+
     var allDirs = files.filter(function(dir) {
-      return fs.statSync(path.join(directory, dir)).isDirectory();
-    })
-    return ignoreDirectories(jsFiles, allDirs, filesToRead);
+      return fs.statSync(path.join(appPath, dir)).isDirectory();
+    });
+    if (allDirs.length > 0)
+      return ignoreDirectories(appPath, allDirs);
+
+    filesArray.forEach(function(file) {
+      return parseFiles.parse(file);
+    });
   });
 }
 
 getFileNames(__dirname);
 
-function ignoreDirectories(jsFiles, allDirs, callback) {
+function ignoreDirectories(appPath, allDirs) {
   fs.readFile(__dirname + '/.gitignore', 'utf8', function(err, data) {
     if (err) throw err;
     var ignoredDirs = data.split('\n')
     ignoredDirs.pop()
     ignoredDirs = ignoredDirs.concat(['.git']);
-    filesToRead(ignoredDirs)
-
+    filterDirectories(appPath, allDirs, ignoredDirs);
   });
 }
 
-function filesToRead(userFiles, userDirs) {
-
+function filterDirectories(appPath, allDirs, ignoredDirs) {
+  var filteredDirs = allDirs.filter(function(dir) {
+    return ignoredDirs.indexOf(dir) === -1;
+  });
+  var directories = filteredDirs.map(function(filePath) {
+    return `${appPath}/${filePath}`
+  })
+  directories.forEach(function(dir) {
+    return getFileNames(dir);
+  });
 }
+
+
 
 module.exports = {
   getFileNames,
